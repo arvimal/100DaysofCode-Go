f55763b03362f29503ab234835714a39e75c1e5b
Vimal A R <arvimal81@gmail.com>
2023-11-26 20:07:04 +0100

    Day 002

--- Day-001/hello_world.go
+++ Day-001/hello_world.go
@@ -1,7 +1,9 @@
 package main
 
 import "fmt"
+import "math"
 
-func main() {
+func main() float {
 	fmt.Println("Starting 100 days of code!")
+  return math.Sin(19)
 }
---
+++ Day-002/main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+const PI = 3.1412
+const B string = "Hello"
+const BEEF, TWO, C = "meat", 2, "see"
+
+const (
+	UNKNOWN = 0
+	FEMALE  = 1
+	MALE    = 2
+)
+
+
+bingo := 10
+
+func main() {
+	var number float32 = 5.2
+	fmt.Println(number)
+	fmt.Println(int(number))
+}
+
